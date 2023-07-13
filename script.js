0f7a9dacdc488d857750488966dc4f920f583f75
criggins44 <128842735+criggins44@users.noreply.github.com>
2023-07-13 16:11:07 -0400

    added messages for guesses that are too low or too high

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //Number Guessing Game
 
 var answer = Math.floor(Math.random() *101);
+var tooLow = document.getElementById('guessResult');
+var tooHigh = document.getElementById('guessResult');
 
 function chooseNumber(){
 	Math.floor(Math.random() *101);
@@ -11,5 +13,13 @@ function guessNumber(){
 
 	if(userGuess < 0 || userGuess > 100){
 		alert('Please enter a value 0-100')
+	}
+
+	else if (userGuess < answer){
+		tooLow.innerHTML = 'Too Low';
+	}
+
+	else if (userGuess > answer){
+		tooHigh.innerHTML = 'Too High';
 	}
 }
