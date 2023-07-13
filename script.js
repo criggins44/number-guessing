b54b5c5c267d8439f3ba623302efe309cbd8076c
criggins44 <128842735+criggins44@users.noreply.github.com>
2023-07-13 00:13:10 -0400

    created the HTML and css for the page

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //Number Guessing Game
 
+var answer = Math.floor(Math.random() *101);
+
+function chooseNumber(){
+	Math.floor(Math.random() *101);
+}
+
+function guessNumber(){
+	userGuess = document.getElementById('guessBox').value;
+
+	if(userGuess < 0 || userGuess > 100){
+		alert('Please enter a value 0-100')
+	}
+}
