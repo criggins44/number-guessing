03ad919880971f6f35c9373960fd18422569f279
criggins44 <128842735+criggins44@users.noreply.github.com>
2023-07-17 11:11:42 -0400

    added a counter in JS and HTML to track the number of guesses

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 //Number Guessing Game
 
-var answer = Math.floor(Math.random() *101);
+var answer = Math.floor(Math.random() * 101);
 var tooLow = document.getElementById('guessResult');
 var tooHigh = document.getElementById('guessResult');
+var correctAnswer = document.getElementById('guessResult');
+var amountOfGuesses = document.getElementById('guessesUsed');
+var numberOfGuesses = 0;
 
 function chooseNumber(){
-	Math.floor(Math.random() *101);
+	Math.floor(Math.random() * 101);
 }
 
 function guessNumber(){
 	userGuess = document.getElementById('guessBox').value;
+	numberOfGuesses +=1;
 
 	if(userGuess < 0 || userGuess > 100){
 		alert('Please enter a value 0-100')
@@ -17,9 +21,15 @@ function guessNumber(){
 
 	else if (userGuess < answer){
 		tooLow.innerHTML = 'Too Low';
+		amountOfGuesses.innerHTML = 'Used Guesses: ' + numberOfGuesses;
 	}
 
 	else if (userGuess > answer){
 		tooHigh.innerHTML = 'Too High';
+		amountOfGuesses.innerHTML = 'Used Guesses: ' + numberOfGuesses;
+	}
+
+	else if (userGuess = answer){
+		correctAnswer.innerHTML = 'CONGRATULATIONS';
 	}
 }
