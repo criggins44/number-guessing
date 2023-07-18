cdf97e1334e7e9a493dd12b356df1ecebc677d49
criggins44 <128842735+criggins44@users.noreply.github.com>
2023-07-18 18:31:08 -0400

    added a guess counter and game over message to HTML and JS

--- script.js
+++ script.js
@@ -1,35 +1,39 @@
 //Number Guessing Game
-
 var answer = Math.floor(Math.random() * 101);
-var tooLow = document.getElementById('guessResult');
-var tooHigh = document.getElementById('guessResult');
-var correctAnswer = document.getElementById('guessResult');
-var amountOfGuesses = document.getElementById('guessesUsed');
-var numberOfGuesses = 0;
+var result = document.getElementById('guessesUsed');
+var feedback = document.getElementById('guessFeedback');
+let numberOfGuesses = 0;
+var guessCounter = 0
 
 function chooseNumber(){
 	Math.floor(Math.random() * 101);
 }
 
 function guessNumber(){
-	userGuess = document.getElementById('guessBox').value;
-	numberOfGuesses +=1;
+	var userGuess = document.getElementById('guessBox').value
 
 	if(userGuess < 0 || userGuess > 100){
-		alert('Please enter a value 0-100')
+		alert('Please enter a value 0 - 100');
 	}
-
-	else if (userGuess < answer){
-		tooLow.innerHTML = 'Too Low';
-		amountOfGuesses.innerHTML = 'Used Guesses: ' + numberOfGuesses;
+	else{
+		if(userGuess < answer){
+			numberOfGuesses ++;
+			guessCounter ++;
+			result.innerHTML = 'Used Guesses: ' + numberOfGuesses;
+			feedback.innerHTML = 'Your guess is: too low';
+		}
+		else if(userGuess > answer){
+			numberOfGuesses ++;
+			guessCounter ++;
+			result.innerHTML = 'Used Guesses: ' + numberOfGuesses;
+			feedback.innerHTML = 'Your guess is: too high';
+		}
+		else if(userGuess = answer){
+			result.innerHTML = 'CONGRATULATIONS';
+		}
 	}
-
-	else if (userGuess > answer){
-		tooHigh.innerHTML = 'Too High';
-		amountOfGuesses.innerHTML = 'Used Guesses: ' + numberOfGuesses;
-	}
-
-	else if (userGuess = answer){
-		correctAnswer.innerHTML = 'CONGRATULATIONS';
-	}
+	 if(guessCounter === 4){
+	 	alert('GAME OVER');
+	 	return;
+	 }
 }
